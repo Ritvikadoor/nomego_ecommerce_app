cf8c9fde3bb91a2a37999899002032c5b02393b5
Ritvik_Pramod <103210186+Ritvikadoor@users.noreply.github.com>
2022-09-20 14:53:20 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:nomego_ecommerce_app/constants/global_variables.dart';
+import 'package:nomego_ecommerce_app/modules/features/auth/screens/auth_screens.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+            scaffoldBackgroundColor: GlobalVariables.backgroundColor,
+            appBarTheme: const AppBarTheme(
+                elevation: 0,
+                iconTheme:
+                    IconThemeData(color: GlobalVariables.greyBackgroundCOlor)),
+            colorScheme: const ColorScheme.light(
+                primary: GlobalVariables.secondaryColor)),
+        home: const AuthScreen());
+  }
+}
