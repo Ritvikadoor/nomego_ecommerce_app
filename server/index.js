602bf56850276c700b5b4279988b5430d9a3bcc6
Ritvik_Pramod <103210186+Ritvikadoor@users.noreply.github.com>
2022-09-21 11:26:03 +0530

    created signup api

--- server/index.js
+++ server/index.js
@@ -5,12 +5,25 @@ const mongoose = require("mongoose");
 //IMPORT FROM OTHER FILES
 const authRouter=require("./routes/auth");
 
-//MIDDLEWARE 
-//CLIENTSIDE->>>>>>SERVER
+
 //INIT
 
 const PORT = process.env.PORT || 3000;
 const app = express();
-app.listen(PORT, () => {
+const DB= "mongodb+srv://ritvik:526621@cluster0.abb5a8a.mongodb.net/?retryWrites=true&w=majority"
+
+//MIDDLEWARE 
+app.use(express.json())
+app.use(authRouter);
+
+//connections
+mongoose.connect(DB)
+.then(()=>{
+    console.log('Connection Succesful');
+}).catch((e)=>{console.log(e);
+})
+//CLIENTSIDE->>>MIDDLE WARA->>>SERVER->>>>>>>>CLIENT
+
+app.listen(PORT, "0.0.0.0",() => {
   console.log(`connected at port ${PORT}`);
 });
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,11 +1,52 @@
 
-
 const express = require("express");
-
-
 const authRouter =express.Router();
+const mongoose= require("mongoose");
+const User = require("../models/user");
+const bcryptjs=require('bcryptjs');
 
-authRouter.get('/user',(req,res)=>{
-    res.json({name:"ritvik"});
+
+
+authRouter.post('/api/signup',async (req,res)=>{
+    try{
+
+
+
+    const {name,email,password} = req.body;
+    const existingUser=await User.findOne({email});
+    if(existingUser){
+     return res.status(400).json({msg:"User with same email already exist!"});
+ 
+    }
+ const hashedPasword=await   bcryptjs.hash(password,8);
+   let user= new User({
+ email,
+ password: hashedPasword,
+ name,
+    })
+ user=await user.save()
+ res.json(user);
+ 
+ 
+ 
+
+    }catch(e){
+        res.status(500).json({error:e.message});
+
+    }
+
+
+
+
+
+
+
 });
+
+
+
+
+
+
+
 module.exports= authRouter;
