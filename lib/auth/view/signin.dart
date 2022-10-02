a0088048eb814326b7a705a6fa301e5e7288dc07
Ritvik_Pramod <103210186+Ritvikadoor@users.noreply.github.com>
2022-10-02 20:31:20 +0530

    completed add to cart

--- lib/auth/view/signin.dart
+++ lib/auth/view/signin.dart
@@ -32,7 +32,7 @@ class SignIn extends StatelessWidget {
                     decoration: const BoxDecoration(
                         image: DecorationImage(
                             image: AssetImage(
-                                "assets/d544ba41-973c-4c06-8b9a-d0fb7a8cd894-removebg-preview.png"))),
+                                "assets/images/d544ba41-973c-4c06-8b9a-d0fb7a8cd894-removebg-preview.png"))),
                   )
                 ],
               ),
